aed1d3276a2f56aa8d8736bbec25ae98dbfc34ab
krushna <ingalekrushna3003@gmail.com>
2024-10-21 16:24:26 +0530

    Category type changed

--- src/components/category/CreateCategory.jsx
+++ src/components/category/CreateCategory.jsx
@@ -12,7 +12,7 @@ function CreateCategory() {
     { label: "No", value: "no" },
     { label: "Yes", value: "yes" },
   ];
-  const categoryType = ["Vlogging", "Electronics", "Videography", "Mobile Accessories", "Sports Fitness"];
+  const categoryType = ["Vlogging", "Electronics", "Camera", "Mobile Accessories", "Sports Fitness"];
   const { register, handleSubmit, getValues, reset, control } = useForm();
   const [colors, setColors] = useState([{ name: "", hexCode: "" }]);
   const [services, SetServices] = useState([{ name: "", cost: "", unitType : "" }]);
